52fa6bc877f3be76be1e755a6a8f8b58182c8dc9
Chris Cummings <ccummings@nvidia.com>
2025-05-29 14:38:47 +0100

    Work on nm course

--- experiments/mipmap/main.py
+++ experiments/mipmap/main.py
@@ -3,15 +3,29 @@
 from app import App
 import slangpy as spy
 import numpy as np
-import sgl
 import sys
 import time
 import math
 from pathlib import Path
 
+MODE_TRAIN = "train"
+MODE_RENDER_FULL_RES = "render_full_res"
+MODE_SIDE_BY_SIDE = "side_by_side"
+MODE = MODE_RENDER_FULL_RES
+
+if MODE == MODE_TRAIN:
+    res = (512, 2048) # (height, width)
+elif MODE == MODE_RENDER_FULL_RES:
+    res = (2048, 2048) # (height, width)
+elif MODE == MODE_SIDE_BY_SIDE:
+    res = (2048, 4096) # (height, width)
+else:
+    raise ValueError(f"Unknown mode: {MODE}")
+
 # Create the app and load the sample shader.
-app = App(width=2048, height=512, title="Mipmap Example")
+app = App(width=res[1], height=res[0], title="Mipmap Example")
 module = spy.Module.load_from_file(app.device, "mipmapping.slang")
+
 
 # The purpose of this example is to render BRDF using input material and
 # normal map textures.
@@ -27,13 +41,13 @@ module = spy.Module.load_from_file(app.device, "mipmapping.slang")
 
 # Generate a UV grid for the window, starting at (0, 0) for the top left pixel
 window_w, window_h = app.window.width, app.window.height
-windowUVs = module.pixelToUV(spy.grid((window_h, window_w)), sgl.int2(window_w, window_h))
+windowUVs = module.pixelToUV(spy.grid((window_h, window_w)), spy.int2(window_w, window_h))
 
 
 # Simple function to read a texture from a file and exit if an error occurs.
-def createTextureFromFile(device: sgl.Device, filepath: str):
+def createTextureFromFile(device: spy.Device, filepath: str):
     try:
-        loader = sgl.TextureLoader(device)
+        loader = spy.TextureLoader(device)
         texture = loader.load_texture(Path(__file__).parent / filepath)
         return texture
 
@@ -67,16 +81,13 @@ def getRandomDir():
     Lx = r * math.sin(phi)
     Ly = r * math.cos(phi)
     Lz = math.sqrt(max(1 - r ** 2, 0))
-    return sgl.float3(Lx, Ly, Lz)
+    return spy.float3(Lx, Ly, Lz)
 
 
 # Material, normal map and roughness textures used for this example.
-albedo_map: spy.Tensor = module.toAlbedoMap(createTextureFromFile(
-    app.device, "PavingStones070_2K.diffuse.jpg"), _result='tensor')
-normal_map: spy.Tensor = module.toNormalMap(createTextureFromFile(
-    app.device, "PavingStones070_2K.normal.jpg"), _result='tensor')
-roughness_map: spy.Tensor = module.toRoughnessMap(createTextureFromFile(
-    app.device, "PavingStones070_2K.roughness.jpg"), _result='tensor')
+albedo_map = spy.Tensor.load_from_image(app.device, Path(__file__).parent / "PavingStones070_2K.diffuse.jpg", linearize=True)
+normal_map = spy.Tensor.load_from_image(app.device, Path(__file__).parent / "PavingStones070_2K.normal.jpg", scale=2, offset=-1)
+roughness_map = spy.Tensor.load_from_image(app.device, Path(__file__).parent / "PavingStones070_2K.diffuse.jpg", greyscale=True)
 
 downsampled_albedo_map = downsampleTensorFloat3(albedo_map)
 downsampled_normal_map = downsampleTensorFloat3(normal_map)
@@ -115,8 +126,8 @@ learning_rate = 0.001
 # In case we want to start training when pressing the 'tab' key.
 #start = False
 #
-#def on_keyboard_event(key: sgl.KeyboardEvent):
-#    if key.type == sgl.KeyboardEventType.key_press and key.key == sgl.KeyCode.tab:
+#def on_keyboard_event(key: spy.KeyboardEvent):
+#    if key.type == spy.KeyboardEventType.key_press and key.key == spy.KeyCode.tab:
 #        global start
 #        start = True
 #
@@ -129,65 +140,119 @@ while app.process_events():
     #if not start:
     #    continue
     # Generate random light and view dirs on the hemisphere.
-    light_dir = getRandomDir()
-    view_dir = getRandomDir()
+    #light_dir = getRandomDir()
+    #view_dir = getRandomDir()
 
     # Alternative: Smooth sweep of light direction.
-    #t = math.sin(time.time()*2)*1
-    #light_dir = sgl.math.normalize(sgl.float3(t, t, 1.0))
-    #view_dir = sgl.float3(0, 0, 1)
+    #t = math.sin(time.time()*0.5)*1
+    #light_dir = spy.math.normalize(spy.float3(t, t, 1.0))
+    #view_dir = spy.float3(0, 0, 1)
 
-    # Full res rendered output BRDF from full res inputs.
-    rendered: spy.Tensor = module.renderFullRes(albedo_map, normal_map,
-        roughness_map, light_dir, view_dir, _result='tensor')
+    light_dir = spy.math.normalize(spy.float3(0.2, 0.2, 1.0))
+    view_dir = spy.float3(0, 0, 1)
 
-    # Downsampled output (avg) from the full res inputs.
-    downsampled = downsampleTensorFloat3(rendered)
+    if MODE == MODE_TRAIN:
 
-    # Lower res BRDF from downsampled inputs (for comparison).
-    lowres: spy.Tensor = module.renderFullRes(downsampled_albedo_map, downsampled_normal_map,
-        downsampled_roughness_map, light_dir, view_dir, _result='tensor')
+        # Full res rendered output BRDF from full res inputs.
+        rendered: spy.Tensor = module.renderFullRes(albedo_map, normal_map,
+            roughness_map, light_dir, view_dir, _result='tensor')
 
-    # Take the function that calculates the loss, i.e. the difference between the downsampled output
-    # and the output calculated with downsampled albedo/normals, and run it 'backwards'
-    # This propagates the gradient of training_loss back to the gradients of trained_normals.
-    module.calculateLoss.bwds(downsampled, downsampled_albedo_map,
-        trained_normals, trained_roughness, light_dir, view_dir, _result=training_loss)
-    # trained_normals.grad_out now tells us how trained_normals needs to change
-    # so that training_loss changes by training_loss.grad_in
+        # Downsampled output (avg) from the full res inputs.
+        downsampled = downsampleTensorFloat3(rendered)
 
-    # We want training_loss to go down, so we subtract a tiny bit of that gradient.
-    # One option is to use gradient descent which is simple but can get stuck in local minima.
-    #module.gradientDescentFloat3(trained_normals, trained_normals.grad, learning_rate)
-    #module.gradientDescentFloat3(trained_roughness, trained_roughness.grad, learning_rate)
-    # In the next iteration, the updated trained_normals now hopefully reduces the loss.
+        # Lower res BRDF from downsampled inputs (for comparison).
+        lowres: spy.Tensor = module.renderFullRes(downsampled_albedo_map, downsampled_normal_map,
+            downsampled_roughness_map, light_dir, view_dir, _result='tensor')
 
-    # Another option is to use an Adam optimizer.
-    module.adamFloat3(trained_normals, trained_normals.grad, m_normal, v_normal, learning_rate)
-    module.adamFloat(trained_roughness, trained_roughness.grad, m_roughness, v_roughness, learning_rate)
+        # Take the function that calculates the loss, i.e. the difference between the downsampled output
+        # and the output calculated with downsampled albedo/normals, and run it 'backwards'
+        # This propagates the gradient of training_loss back to the gradients of trained_normals.
+        module.calculateLoss.bwds(downsampled, downsampled_albedo_map,
+            trained_normals, trained_roughness, light_dir, view_dir, _result=training_loss)
+        # trained_normals.grad_out now tells us how trained_normals needs to change
+        # so that training_loss changes by training_loss.grad_in
 
-    # This is just here for debugging purposes, to confirm the data looks correct.
-    #iter += 1
-    #if iter % 50 == 0:
-    #    iter = 0
-    #    resultArray = training_loss.to_numpy()
-    #    loss = np.sum(resultArray) / resultArray.size
-    #    print("Iteration: {}, Loss: {}".format(iter, loss))
-    #    print("parameter {}".format(trained_normals.to_numpy()))
+        # We want training_loss to go down, so we subtract a tiny bit of that gradient.
+        # One option is to use gradient descent which is simple but can get stuck in local minima.
+        #module.gradientDescentFloat3(trained_normals, trained_normals.grad, learning_rate)
+        #module.gradientDescentFloat3(trained_roughness, trained_roughness.grad, learning_rate)
+        # In the next iteration, the updated trained_normals now hopefully reduces the loss.
 
-    # Render current progress.
-    loss: spy.Tensor = module.renderLoss(
-        downsampled, downsampled_albedo_map, trained_normals, trained_roughness, light_dir, view_dir, _result='tensor')
-    result: spy.Tensor = module.renderFullRes(
-        downsampled_albedo_map, trained_normals, trained_roughness, light_dir, view_dir, _result='tensor')
+        # Another option is to use an Adam optimizer.
+        module.adamFloat3(trained_normals, trained_normals.grad, m_normal, v_normal, learning_rate)
+        module.adamFloat(trained_roughness, trained_roughness.grad, m_roughness, v_roughness, learning_rate)
 
-    # Compare the loss between the trained result and the lowres result.
-    # Green pixels indicate a better result, and red pixels indicate a worse
-    # result.
-    loss_lowres: spy.Tensor = module.lossLowres(downsampled, lowres, _result='tensor')
-    loss_diff: spy.Tensor = module.lossDiff(loss, loss_lowres, _result='tensor')
+        # This is just here for debugging purposes, to confirm the data looks correct.
+        #iter += 1
+        #if iter % 50 == 0:
+        #    iter = 0
+        #    resultArray = training_loss.to_numpy()
+        #    loss = np.sum(resultArray) / resultArray.size
+        #    print("Iteration: {}, Loss: {}".format(iter, loss))
+        #    print("parameter {}".format(trained_normals.to_numpy()))
 
-    module.showTrainingProgress(result, loss_diff, downsampled_normal_map,
-        trained_normals_without_grads, trained_roughness_without_grads, windowUVs, _result=app.output)
+        # Render current progress.
+        loss: spy.Tensor = module.renderLoss(
+            downsampled, downsampled_albedo_map, trained_normals, trained_roughness, light_dir, view_dir, _result='tensor')
+        result: spy.Tensor = module.renderFullRes(
+            downsampled_albedo_map, trained_normals, trained_roughness, light_dir, view_dir, _result='tensor')
+
+        # Compare the loss between the trained result and the lowres result.
+        # Green pixels indicate a better result, and red pixels indicate a worse
+        # result.
+        loss_lowres: spy.Tensor = module.lossLowres(downsampled, lowres, _result='tensor')
+        loss_diff: spy.Tensor = module.lossDiff(loss, loss_lowres, _result='tensor')
+
+        module.showTrainingProgress(result, loss_diff, downsampled_normal_map,
+            trained_normals_without_grads, trained_roughness_without_grads, windowUVs, _result=app.output)
+
+    elif MODE == MODE_RENDER_FULL_RES:
+        # Full res rendered output BRDF from full res inputs.
+        rendered: spy.Tensor = module.renderFullRes(albedo_map, normal_map,
+            roughness_map, light_dir, view_dir, _result='tensor')
+
+        # Blit to screen
+        size = spy.int2(app.output.desc.width, app.output.desc.height)
+        module.blit(spy.grid((size.y,size.x)),
+                    size,
+                    spy.int2(0),
+                    rendered,
+                    app.output)
+
+    elif MODE == MODE_SIDE_BY_SIDE:
+        # Full res rendered output BRDF from full res inputs.
+        rendered: spy.Tensor = module.renderFullRes(albedo_map, normal_map,
+            roughness_map, light_dir, view_dir, _result='tensor')
+
+        # Downsampled output (avg) from the full res inputs.
+        downsampled = downsampleTensorFloat3(rendered)
+
+        # Blit to screen
+        size = spy.int2(app.output.desc.width // 2, app.output.desc.height)
+        size.x -= 10
+        size.y -= 10
+        module.blit(spy.grid((size.y,size.x)),
+                    size,
+                    spy.int2(0),
+                    downsampled,
+                    app.output)
+
+        # Lower res BRDF from downsampled inputs (for comparison).
+        lowres: spy.Tensor = module.renderFullRes(downsampled_albedo_map, downsampled_normal_map,
+            downsampled_roughness_map, light_dir, view_dir, _result='tensor')
+
+        module.blit(spy.grid((size.y,size.x)),
+                    size,
+                    spy.int2(app.output.desc.width//2,0),
+                    lowres,
+                    app.output)
+
+        # Blit the downsampled result to the right side of the screen.
+        #downsampled_size = spy.int2(size.x // 2, size.y)
+        #module.blit(spy.grid((downsampled_size.y, downsampled_size.x)),
+        #            downsampled_size,
+        #            spy.int2(size.x // 2, 0),
+        #            downsampled_albedo_map,
+        #            app.output)
 
     app.present()
---
+++ experiments/mipmap/nsc_02_downsample.py
@@ -0,0 +1,49 @@
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+from app import App
+import slangpy as spy
+
+# Create the app and load the slang module.
+app = App(width=2048, height=2048, title="Mipmap Example")
+module = spy.Module.load_from_file(app.device, "nsc_02_downsample.slang")
+
+# Load some materials.
+albedo_map = spy.Tensor.load_from_image(app.device,
+                                        "PavingStones070_2K.diffuse.jpg", linearize=True)
+normal_map = spy.Tensor.load_from_image(app.device,
+                                        "PavingStones070_2K.normal.jpg", scale=2, offset=-1)
+
+def downsample(source: spy.Tensor, steps: int) -> spy.Tensor:
+    for i in range(steps):
+        dest = spy.Tensor.empty(device=app.device, shape=(source.shape[0] // 2, source.shape[1] // 2), dtype=source.dtype)
+        module.downsample(spy.call_id(), source, _result=dest)
+        source = dest
+    return source
+
+while app.process_events():
+
+    # Allocate a tensor for output
+    output = spy.Tensor.empty_like(albedo_map)
+
+    # Full res rendered output BRDF from full res inputs.
+    module.render(pixel = spy.call_id(),
+                  material = {
+                        "albedo": albedo_map,
+                        "normal": normal_map,
+                  },
+                  light_dir = spy.math.normalize(spy.float3(0.2, 0.2, 1.0)),
+                  view_dir = spy.float3(0, 0, 1),
+                  _result = output)
+
+    # Downsample the output tensor.
+    output = downsample(output, 2)
+
+    # Blit tensor to screen.
+    app.blit(output, size=spy.int2(2048,2048))
+
+    # Present the window.
+    app.present()
+
+
+
+

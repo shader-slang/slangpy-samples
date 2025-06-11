95fe6ee89a366c905a1681bd9c5ae4e753e48d29
Chris Cummings <ccummings@nvidia.com>
2025-06-11 14:53:48 +0100

    Optimizer improvements to support unbatched params

--- experiments/neuralnetwork/neuralnetworks/optimizers/Optimizer.py
+++ experiments/neuralnetwork/neuralnetworks/optimizers/Optimizer.py
@@ -44,8 +44,10 @@ class OptimizerPool:
         self.step_func = step_func
         self.batch_step_func = batch_step_func
         self.params: list[Tensor] = []
+        self.states: list[NDBuffer] = []
         self.mapping = np.ndarray((0,2), dtype=np.int32)
-        self.batches: list[dict[str,Any]] = []
+        self.batched_params: list[dict[str,Any]] = []
+        self.unbatched_params: list[dict[str,Any]] = []
 
     def finalise(self):
         """
@@ -57,40 +59,50 @@ class OptimizerPool:
         self.mapping_buffer.copy_from_numpy(self.mapping)
 
         # Create the packed batch data
-        self.batches_packed = pack(self.optim_type.module, self.batches)
+        self.batches_packed = pack(self.optim_type.module, self.batched_params)
 
         # Workaround for Slang reflection issue - explicitly specialize batch_step
         # for the number of batches we have, so that Slang can find the correct function.
-        self.batch_step_func = self.optim_type.module.find_function_in_struct(self.optim_type, f"batch_step<{len(self.batches)}>")
+        self.batch_step_func = self.optim_type.module.find_function_in_struct(self.optim_type, f"batch_step<{len(self.batched_params)}>")
 
-    def add_parameter(self, param: Tensor, existing_state: Optional[Buffer] = None):
+    def add_parameter(self, param: Tensor, existing_state: Optional[NDBuffer] = None):
         """
         Adds a parameter to the optimizer pool.
         """
 
-        param_idx = len(self.params)
         self.params.append(param)
 
         # If no existing state is provided, create a new state for the parameter
         if existing_state is None:
-            state = self.state_type(param).storage
+            state = self.state_type(param)
         else:
-            assert existing_state.size == param.element_count * self.state_type.buffer_layout.stride
+            assert existing_state.element_count == param.element_count
             state = existing_state
+        self.states.append(state)
 
-        self.batches.append(InstanceList(self.batch_type,{
-            "params": param.detach().storage,
-            "grads": param.grad.storage,
-            "states": state,
-        }))
+        if param.element_count >= 32*1024:
+            # If parameter is large enough, it's suitable for a single dispatch
+            self.unbatched_params.append({
+                "params": param.detach(),
+                "grads": param.grad,
+                "states": state,
+            })
+        else:
+            # Small parameters are batched together into 1 mega dispatch with an LUT
+            batch_idx = len(self.batched_params)
+            self.batched_params.append(InstanceList(self.batch_type,{
+                "params": param.storage,
+                "grads": param.grad.storage,
+                "states": state.storage,
+            }))
 
-        # Append to the mapping array 1 entry for each element of the tensor,
-        # where the entry is [param_idx, element_idx]
-        new_mapping = np.column_stack((
-            np.full(param.element_count, param_idx, dtype=np.int32),
-            np.arange(param.element_count, dtype=np.int32)
-        ))
-        self.mapping = np.vstack([self.mapping, new_mapping])
+            # Append to the mapping array 1 entry for each element of the tensor,
+            # where the entry is [param_idx, element_idx]
+            new_mapping = np.column_stack((
+                np.full(param.element_count, batch_idx, dtype=np.int32),
+                np.arange(param.element_count, dtype=np.int32)
+            ))
+            self.mapping = np.vstack([self.mapping, new_mapping])
 
     def prune(self, parameters_to_keep: set[Tensor]):
         """
@@ -98,11 +110,13 @@ class OptimizerPool:
         This is called when the optimizer is no longer used.
         """
         curr_params = self.params
-        curr_states = [x.states for x in self.batches]
+        curr_states = self.states
 
         self.params = []
+        self.states = []
         self.mapping = np.ndarray((0,2), dtype=np.int32)
-        self.batches = []
+        self.batched_params = []
+        self.unbatched_params = []
 
         for (param,state) in zip(curr_params, curr_states):
             if param in parameters_to_keep:
@@ -183,8 +197,13 @@ class Optimizer:
         for pool in self.pools.values():
             if cmd is None:
                 pool.batch_step_func(this, pool.batches_packed, pool.mapping_buffer)
+                for param in pool.unbatched_params:
+                    pool.step_func(this, param['states'], param['params'], param['grads'])
             else:
                 pool.batch_step_func.append_to(cmd, this, pool.batches_packed, pool.mapping_buffer)
+                for param in pool.unbatched_params:
+                    pool.step_func.append_to(cmd, this, param['states'], param['params'], param['grads'])
+
 
     def get_type_name(self, dtype: Real) -> str:
         """Returns the name of a slang type implementing IOptimizer<dtype>"""

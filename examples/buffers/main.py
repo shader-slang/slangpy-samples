bf9fe1ca46748404923974c104503329620b2162
Simon Kallweit <skallweit@nvidia.com>
2025-07-08 17:39:31 +0200

    fix displaying result

--- examples/buffers/main.py
+++ examples/buffers/main.py
@@ -49,7 +49,5 @@ for x in range(16):
         print(f"Pixel ({x},{y}): {pixel}")
 
 # Or if installed, we can use tev to show the result (https://github.com/Tom94/tev)
-tex = device.create_texture(
-    data=result.to_numpy(), width=16, height=16, format=spy.Format.rgba32_float
-)
-spy.tev.show(tex)
+bitmap = spy.Bitmap(data=result.to_numpy().view(np.float32))
+spy.tev.show(bitmap)

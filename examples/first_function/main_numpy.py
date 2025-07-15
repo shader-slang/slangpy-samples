e16a58b0c7a9be1b67fb9711ea01f0d795780acd
James Helferty <jhelferty@nvidia.com>
2025-07-15 10:57:06 -0400

    Updates to match documentation

--- examples/first_function/main_numpy.py
+++ examples/first_function/main_numpy.py
@@ -7,22 +7,22 @@ import slangpy as spy
 import pathlib
 import numpy as np
 
-# Create a device with the local folder for slangpy includes
+# Create a SlangPy device; it will look in the local folder for any Slang includes
 device = spy.create_device(
     include_paths=[
         pathlib.Path(__file__).parent.absolute(),
     ]
 )
 
-# Load module
+# Load the module
 module = spy.Module.load_from_file(device, "example.slang")
 
-# Create a couple of buffers with 1,000,000 random floats in
+# Create a couple of buffers containing 1,000,000 random floats
 a = np.random.rand(1000000).astype(np.float32)
 b = np.random.rand(1000000).astype(np.float32)
 
-# Call our function and ask for a numpy array back (the default would be a buffer)
+# Call our function and request a numpy array as the result (the default would be a buffer)
 result = module.add(a, b, _result="numpy")
 
-# Print the first 10
+# Print the first 10 results
 print(result[:10])

fe207b195876d702fc6914d4b8cc6e28e3697898
Shannon Woods <shannon@shewritescode.net>
2025-07-17 11:58:57 -0400

    Add wave ballot example (#7)
    * Add a wave-ballot version of the 2D diff splatting experiment
    
    * Updates balloted splatting example to declare its capability requirements.
    
    Also removes sgl import/dependency.
    
    * Update whitespace for capability tokens
    
    Co-authored-by: ArielG-NV <159081215+ArielG-NV@users.noreply.github.com>
    
    * Remove capability requirements
    
    The Slang compiler will automatically enforce requirements, no need to annotate.
    
    * Incorporate call group shape changes
    
    Changes originally authored by Sam Ricker, sricker@nvidia.com
    
    * Remove erroneously added pycache file
    
    * Use default calling convention for callshapeinfo member
    
    ---------
    
    Co-authored-by: Shannon Woods <158105547+swoods-nv@users.noreply.github.com>
    Co-authored-by: ArielG-NV <159081215+ArielG-NV@users.noreply.github.com>

---
+++ experiments/balloted-splatting/app.py
@@ -0,0 +1,121 @@
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+from typing import Callable, Optional
+import slangpy as spy
+from pathlib import Path
+
+
+class App:
+    def __init__(self, title="Balloted Splat Example", width=1024, height=1024, device_type=spy.DeviceType.d3d12):
+        super().__init__()
+
+        # Create a window
+        self._window = spy.Window(
+            width=width, height=height, title=title, resizable=True
+        )
+
+        # Create a device with local include path for shaders
+        self._device = spy.create_device(device_type,
+                                             include_paths=[Path(__file__).parent])
+
+        # Setup swapchain
+        self.surface = self._device.create_surface(self._window)
+        self.surface.configure(width=self._window.width, height=self._window.height, format=spy.Format.rgba8_unorm)
+
+        self._output_texture: spy.Texture = self.device.create_texture(
+            width=self._window.width,
+            height=self._window.height,
+            format=spy.Format.rgba32_float,
+            usage=spy.TextureUsage.shader_resource | spy.TextureUsage.unordered_access,
+            label="output_texture",
+        )
+
+        # Store mouse pos
+        self._mouse_pos = spy.float2()
+
+        # Internal events
+        self._window.on_keyboard_event = self._on_window_keyboard_event
+        self._window.on_mouse_event = self._on_window_mouse_event
+        self._window.on_resize = self._on_window_resize
+
+        # Hookable events
+        self.on_keyboard_event: Optional[Callable[[spy.KeyboardEvent], None]] = None
+        self.on_mouse_event: Optional[Callable[[spy.MouseEvent], None]] = None
+
+    @property
+    def device(self) -> spy.Device:
+        return self._device
+
+    @property
+    def window(self) -> spy.Window:
+        return self._window
+
+    @property
+    def mouse_pos(self) -> spy.float2:
+        return self._mouse_pos
+
+    @property
+    def output(self) -> spy.Texture:
+        return self._output_texture
+
+    def process_events(self):
+        if self._window.should_close():
+            return False
+        self._window.process_events()
+        return True
+
+    def present(self):
+        image = self.surface.acquire_next_image()
+        if image is None:
+            return
+
+        if (self._output_texture == None
+                or self._output_texture.width != image.width
+                or self._output_texture.height != image.height
+            ):
+            self._output_texture = self.device.create_texture(
+                width=image.width,
+                height=image.height,
+                format=spy.Format.rgba32_float,
+                usage=spy.TextureUsage.shader_resource | spy.TextureUsage.unordered_access,
+                label="output_texture",
+            )
+
+        command_buffer = self._device.create_command_encoder()
+        command_buffer.blit(image, self._output_texture)
+        command_buffer.set_texture_state(image, spy.ResourceState.present)
+        self._device.submit_command_buffer(command_buffer.finish())
+
+        del image
+        self.surface.present()
+
+    def _on_window_keyboard_event(self, event: spy.KeyboardEvent):
+        if event.type == spy.KeyboardEventType.key_press:
+            if event.key == spy.KeyCode.escape:
+                self._window.close()
+                return
+            elif event.key == spy.KeyCode.f1:
+                if self._output_texture:
+                    spy.tev.show_async(self._output_texture)
+                return
+            elif event.key == spy.KeyCode.f2:
+                if self._output_texture:
+                    bitmap = self._output_texture.to_bitmap()
+                    bitmap.convert(
+                        spy.Bitmap.PixelFormat.rgb,
+                        spy.Bitmap.ComponentType.uint8,
+                        srgb_gamma=True,
+                    ).write_async("screenshot.png")
+                return
+        if self.on_keyboard_event:
+            self.on_keyboard_event(event)
+
+    def _on_window_mouse_event(self, event: spy.MouseEvent):
+        if event.type == spy.MouseEventType.move:
+            self._mouse_pos = event.pos
+        if self.on_mouse_event:
+            self.on_mouse_event(event)
+
+    def _on_window_resize(self, width: int, height: int):
+        self._device.wait()
+        self.surface.configure(width=width, height=height)

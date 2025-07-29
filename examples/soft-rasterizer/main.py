bd4879c30d33f47f443b062ecb31c4ef841165f5
James Helferty <jhelferty@nvidia.com>
2025-07-29 11:42:29 -0400

    soft rasterizer doc comments

--- examples/soft-rasterizer/main.py
+++ examples/soft-rasterizer/main.py
@@ -5,54 +5,80 @@ import slangpy as spy
 import numpy as np
 from slangpy.types import call_id
 
-# This Camera class mimics the Camera struct from rasterizer2d.slang, setting
-# the origin, scale, and window size that the Slang camera struct expects. The
-# window size is retrieved from the app window to avoid redundant state.
-
 
 class Camera:
-    # Origin is at 0,0 with 1,1 scale.
-    def __init__(self, app):
-        self.o = spy.float2(0.0, 0.0)
+    """
+    This Camera class mimics the Camera struct from rasterizer2d.slang, setting
+    the origin, scale, and window size that the Slang camera struct expects. The
+    window size is retrieved from the app window to avoid redundant state.
+
+    Attributes:
+        origin (slangpy.float2): origin in worldspace
+        scale (slangpy.float2): scale in worldspace
+        app (App): assiciated App
+    """
+
+    def __init__(self, app: App):
+        """
+        Initialize the Camera with origin at (0,0) and scale (1,1)
+
+        Args:
+            app (App): App to retrieve window size from
+        """
+        self.origin = spy.float2(0.0, 0.0)
         self.scale = spy.float2(1.0, 1.0)
         self.app = app
 
-    # Return a dict with the class variables mapped to the names that the
-    # Slang struct expects.
     def get_this(self):
+        """
+        Return a dict with the Camera attributes mapped to names that the
+        Slang struct expects.
+        """
         return {
-            "o": self.o,
+            "origin": self.origin,
             "scale": self.scale,
             "frameDim": spy.float2(self.app._window.width, self.app._window.height),
             "_type": "Camera",
         }
 
 
-# Create app and load the rasterizer2d shader.
-app = App()
-rasterizer2d = spy.Module.load_from_file(app.device, "rasterizer2d.slang")
+def main():
+    """
+    Main function that
+    1. Creates the App window, sets up the GPU device and loads the Slang shader module
+    2. Prepares a buffer with vertices for a single tringle
+    3. Rasterizes the triangle using the Slang shader module
+    """
 
-# Buffer of 3 vertex positions for the triangle.
-vertices = [
-    spy.float2(-0.75, 0.75),
-    spy.float2(-0.75, -0.75),
-    spy.float2(0.75, -0.75),
-]
+    # Create app and load the rasterizer2d shader.
+    app = App()
+    rasterizer2d = spy.Module.load_from_file(app.device, "rasterizer2d.slang")
 
-# Setup the camera with the app's frame dimensions.
-camera = Camera(app)
+    # Buffer of 3 vertex positions for the triangle.
+    vertices = [
+        spy.float2(-0.75, 0.75),
+        spy.float2(-0.75, -0.75),
+        spy.float2(0.75, -0.75),
+    ]
 
-# Run the app.
-currIter = 0
-maxIter = 400
-while app.process_events():
-    # Call the rasterize function in Slang, passing the camera and vertices
-    # array. We also use call_id to pass the pixel coordinate within window
-    # width and height, and set result to render the output color returned by
-    # the rasterize function.
-    #
-    # Unlike the fwd-rasterize version, this function calculates a per-pixel
-    # probability values to determine if a pixel is inside the triangle, and
-    # can be more easily extended to a trained version.
-    rasterizer2d.rasterize(camera, vertices, call_id(), _result=app.output)
-    app.present()
+    # Setup the camera with the app's frame dimensions.
+    camera = Camera(app)
+
+    # Run the app.
+    currIter = 0
+    maxIter = 400
+    while app.process_events():
+        # Call the rasterize function in Slang, passing the camera and vertices
+        # array. We also use call_id to pass the pixel coordinate within window
+        # width and height, and set result to render the output color returned by
+        # the rasterize function.
+        #
+        # Unlike the fwd-rasterize version, this function calculates a per-pixel
+        # probability values to determine if a pixel is inside the triangle, and
+        # can be more easily extended to a trained version.
+        rasterizer2d.rasterize(camera, vertices, call_id(), _result=app.output)
+        app.present()
+
+
+if __name__ == "__main__":
+    main()
